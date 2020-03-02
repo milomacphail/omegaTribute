481d731c1c54b650acfb0ffa50b477915a4ad07b
milomacphail <milomacphail@gmail.com>
2020-03-02 18:26:02 -0500

    Menu fixed

--- script.js
+++ script.js
@@ -1,43 +1,11 @@
-/* Set the width of the side navigation to 275px */
+/* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("sideNavBar").style.width = "10%";
-  }
-
-  function closeNav(){
-    document.getElementById('sideNavBar').style.width = '0%';
-  }
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("open-modal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+  document.getElementById("navbar").style.width = "100%";
+  document.getElementById("landing-text").style.opacity = "0";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-function myFunction() {
-  var x = document.getElementById("sideNavBar");
-  if (x.className === "sidenav") {
-    x.className += " responsive";
-  } else {
-    x.className = "sidenav";
-  }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("navbar").style.width = "0%";
+  document.getElementById("landing-text").style.opacity = "1";
 }
