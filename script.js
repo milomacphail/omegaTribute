d63d6b95778c941ed7808c941f8572ff937b5e23
milomacphail <milomacphail@gmail.com>
2020-03-02 21:22:27 -0500

    Menu overlay fixed - gallery needs work

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
   document.getElementById("navbar").style.width = "100%";
-  document.getElementById("landing-text").style.opacity = "0";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("navbar").style.width = "0%";
-  document.getElementById("landing-text").style.opacity = "1";
 }
