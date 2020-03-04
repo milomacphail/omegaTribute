8c2e33ad3247a25ffc01ebd7f0ce6c58bde9956d
milomacphail <milomacphail@gmail.com>
2020-03-04 08:04:47 -0500

    Static version good to rolle

--- test-scripts.js
+++ test-scripts.js
@@ -2,11 +2,12 @@
 function openNav() {
   document.getElementById("navbar").style.width = "100%";
   document.getElementById("landing-text").style.opacity = "0.3";
+  document.getElementById("form-container").style.opacity = "0.1";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("navbar").style.width = "0%";
   document.getElementById("landing-text").style.opacity = "1";
-  
+  document.getElementById("form-container").style.opacity = "1";
 }
